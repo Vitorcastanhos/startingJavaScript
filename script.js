f402fdc30c7a16d9b1d8e0324bffce46d8b79a39
vitor <vitorcastanhos@gmail.com>
2023-12-22 11:54:46 -0300

    just exercises

---
+++ script.js
@@ -0,0 +1,61 @@
+// document.querySelector("#root").innerHTML = "<h1><u>Hey there</u></h1>";
+
+// const age = prompt ("How old are you?");
+
+// alert (age + " - " + "You are old enough");
+
+// ----------------------------------------------------------------
+// let numberOne = prompt("Type a number...");
+// let numberTwo = prompt("Type another number...");
+
+// let sum = Number(numberOne) + Number(numberTwo);
+
+// alert("The result is: " + sum);
+
+// ----------------------------------------------------------------
+// let numberOne = prompt("Type a number...");
+// let numberTwo = prompt("Type another number...");
+
+// let sum1 = Number(numberOne) + Number(numberTwo);
+// alert(`The result of the adition is: ${sum1}`);
+
+// let sum2 = Number(numberOne) - Number(numberTwo);
+// alert(`The result of the subtraction is: ${sum2}`);
+
+// let sum3 = Number(numberOne) * Number(numberTwo);
+// alert(`The result of the multiplication is: ${sum3}`);
+
+// let sum4 = Number(numberOne) / Number(numberTwo);
+// alert(`The result of the division is: ${sum4}`);
+
+// let sum5 = Number(numberOne) % Number(numberTwo);
+// alert(`The result of the rest is: ${sum5}`);
+
+// ----------------------------------------------------------------
+// let names = prompt("Enter your name: ");
+// let note1 = prompt("Enter your first note: ");
+// let note2 = prompt("Enter your second note: ");
+// let note3 = prompt("Enter your third note: ");
+
+// let sum = (Number(note1) + Number(note2) + Number(note3)) / 3;
+
+// sum = sum.toFixed(1);
+
+// if (sum >= 6) {
+//   alert(`Hi ${names}, you were accepted with: ${sum} points total.`);
+// } else {
+//   alert("Hi " + names + ", you were rejected with: " + sum + " points total");
+// }
+
+// ----------------------------------------------------------------
+// let sip = [];
+
+// for (let i = 0; i <= 2; i++) {
+//   let prp = prompt("type...");
+
+//   sip[i] = prp;
+// }
+
+// alert(sip.join(", "));
+
+// ----------------------------------------------------------------
