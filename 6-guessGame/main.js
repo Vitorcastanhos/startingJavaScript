d2db005ece2efd148ea6d0ca276f93c7ada4b603
vitor <vitorcastanhos@gmail.com>
2024-01-19 11:47:15 -0300

    guessgame completed successfully

--- 6-guessGame/main.js
+++ 6-guessGame/main.js
@@ -4,19 +4,22 @@ let result = prompt("Guess the number im thinking between 0 and 10.");
 // lógica para gerar número aleatório.
 let randomNumber = Math.round(Math.random() * 10);
 
-let match = Number(result) == randomNumber;
-
 // contador de tentativas.
 let count = 1;
 
 // while loop para continuar a contagem até que o número seja encontrado.
 while (Number(result) != randomNumber) {
-	result = prompt("Wrong, try again.");
-	let randomNumber = Math.round(Math.random() * 10);
+	result = Number(prompt("Wrong, try again."));
+	randomNumber = Math.round(Math.random() * 10);
 	count++;
+
+	// variavel para obter valor booleano.
+	let match = Number(result) == randomNumber;
 
 	console.log(match, randomNumber, result);
 }
-
 // alerta para informar que houve acertos.
-alert("Congrats!");
+if (count <= 1) {
+	alert(`Congrats! You got the number with ${count} attempt.`);
+}
+alert(`Congrats! You got the number with ${count} attempts.`);
