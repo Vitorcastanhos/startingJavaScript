9bff87c7fd302fdc565f2f513dfcfa1f3ac4423d
vitor <vitorcastanhos@gmail.com>
2024-01-19 11:54:29 -0300

    guess game instructions added

--- 6-guessGame/main.js
+++ 6-guessGame/main.js
@@ -1,3 +1,18 @@
+/* 
+** Jogo da advinhação **
+
+Apresente a mensagem ao usuário:
+"advinhe o número que estou pensando. está entre 0 e 10"
+
+Crie uma lógica para gerar um número aleatório e verificar se o número digitado 
+é o mesmo que o aleatório gerado pelo sistema.
+
+Enquanto o usuário não adivinhar o número, mostrar a mensagem: "Erro, tente novamente"
+
+Caso o usuário acerte o número, apresentar a mensagem: "parabéns! Você adivinhou o número em x tentativas"
+
+Substitua o x da mensagem, pelo número de tentativas.*/
+
 // mensage para o usuário interagir e digitar seu número.
 let result = prompt("Guess the number im thinking between 0 and 10.");
 
