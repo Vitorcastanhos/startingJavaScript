b7f8e6bfa5d3caef1e636858468073e721fb732c
vitor <vitorcastanhos@gmail.com>
2024-01-20 00:22:05 -0300

    instructions

--- 7-interactingWithMenuOptions/main.js
+++ 7-interactingWithMenuOptions/main.js
@@ -1,3 +1,20 @@
+/*
+Faça um programa que tenha um menu e aoresente a seguinte mensagem: 
+Olá usuário! Digite o número da opção desejada
+
+1.cadastrar um item na lista
+2. mostrar itens cadastrados
+3. sair do programa
+
+o programa deverá capturar o número digitado pelo usuário e mostrar os seguintes cenários:
+
+Caso o usuário digite 1, ele poderá cadastrar um item em uma lista.
+Caso o usuário digite 2, ele poderá ver os items cadastrados.
+Se não houver nenhum item cadastrado, mostrar a mensagem:
+	"não existem items cadastrados"
+Caso o usuário digite 3, a aplicação deverá ser encerrada.
+*/
+
 let options;
 let items = [];
 
